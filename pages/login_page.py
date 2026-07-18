04f17286034df3b0925cd601e3307102847ea7a6
kuzka1287-hue <kuzka1287@gmail.com>
2026-07-18 10:58:01 +0300

    Update login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,14 +1,9 @@
 # pages/login_page.py
-
-from selenium.webdriver.common.by import By
 from pages.base_page import BasePage
+from locators.login_page_locators import LoginPageLocators
 
 class LoginPage(BasePage):
-    EMAIL_INPUT = (By.XPATH, "//input[@name='email']")
-    PASSWORD_INPUT = (By.XPATH, "//input[@name='password']")
-    LOGIN_BUTTON = (By.XPATH, "//button[contains(text(), 'Войти')]")
-
-    def login(self, email, password):
-        self.find_element(self.EMAIL_INPUT).send_keys(email)
-        self.find_element(self.PASSWORD_INPUT).send_keys(password)
-        self.click(self.LOGIN_BUTTON)
+    def login(self, email: str, password: str):
+        self.find_element(LoginPageLocators.EMAIL_INPUT).send_keys(email)
+        self.find_element(LoginPageLocators.PASSWORD_INPUT).send_keys(password)
+        self.click(LoginPageLocators.LOGIN_BUTTON)
