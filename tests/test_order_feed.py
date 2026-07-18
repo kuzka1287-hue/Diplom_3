dc105435967c8325af68e722cc68d9a1f6640796
kuzka1287-hue <kuzka1287@gmail.com>
2026-07-18 22:21:09 +0300

    Update test_order_feed.py

--- tests/test_order_feed.py
+++ tests/test_order_feed.py
@@ -1,8 +1,8 @@
 import allure
-import requests
 from pages.main_page import MainPage
 from pages.orders_feed_page import OrdersFeedPage
-from data import BASE_URL, ORDERS_ENDPOINT, INGREDIENT_IDS
+from data import INGREDIENT_IDS
+from helpers import create_order
 
 @allure.feature("Лента заказов")
 class TestOrderFeed:
@@ -13,13 +13,11 @@ class TestOrderFeed:
         main_page.go_to_orders_feed()
         feed_page = OrdersFeedPage(logged_in_driver)
         initial_total = feed_page.get_total_orders_count()
-        main_page.go_to_constructor()
+
+        # Создаём заказ через API (без проверки статуса в тесте)
         token = main_page.get_cookie_value("accessToken")
-        headers = {"Authorization": f"Bearer {token}"} if token else {}
-        response = requests.post(BASE_URL + ORDERS_ENDPOINT,
-                                 json={"ingredients": INGREDIENT_IDS},
-                                 headers=headers)
-        assert response.status_code == 200, "Не удалось создать заказ через API"
+        create_order(token, INGREDIENT_IDS)
+
         main_page.refresh_page()
         main_page.go_to_orders_feed()
         new_total = feed_page.get_total_orders_count()
@@ -31,13 +29,10 @@ class TestOrderFeed:
         main_page.go_to_orders_feed()
         feed_page = OrdersFeedPage(logged_in_driver)
         initial_today = feed_page.get_today_orders_count()
-        main_page.go_to_constructor()
+
         token = main_page.get_cookie_value("accessToken")
-        headers = {"Authorization": f"Bearer {token}"} if token else {}
-        response = requests.post(BASE_URL + ORDERS_ENDPOINT,
-                                 json={"ingredients": INGREDIENT_IDS},
-                                 headers=headers)
-        assert response.status_code == 200, "Не удалось создать заказ через API"
+        create_order(token, INGREDIENT_IDS)
+
         main_page.refresh_page()
         main_page.go_to_orders_feed()
         new_today = feed_page.get_today_orders_count()
@@ -48,11 +43,13 @@ class TestOrderFeed:
         main_page = MainPage(logged_in_driver)
         main_page.go_to_orders_feed()
         feed_page = OrdersFeedPage(logged_in_driver)
+
         main_page.go_to_constructor()
         main_page.add_ingredient_to_order("Говяжий метеорит")
         main_page.add_ingredient_to_order("Соус традиционный")
         main_page.place_order()
         order_number = main_page.get_order_number_after_creation()
+
         main_page.go_to_orders_feed()
         orders_in_work = feed_page.get_orders_in_work()
         assert str(order_number) in orders_in_work, \
