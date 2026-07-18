bdbd68631383cc45b188bf04c85c93ef65ae4a2f
kuzka1287-hue <kuzka1287@gmail.com>
2026-07-18 22:18:34 +0300

    Update main_page.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,4 +1,3 @@
-from selenium.webdriver.common.by import By
 from pages.base_page import BasePage
 from pages.modal_window import ModalWindow
 from locators.main_page_locators import MainPageLocators
@@ -11,17 +10,17 @@ class MainPage(BasePage):
         self.click(MainPageLocators.ORDERS_FEED_BUTTON)
 
     def click_ingredient(self, ingredient_name: str):
-        ingredient_locator = (By.XPATH, f"//div[contains(@class, 'ingredient')]//p[text()='{ingredient_name}']/..")
-        self.find_element(ingredient_locator).click()
+        locator = MainPageLocators.ingredient_locator(ingredient_name)
+        self.find_element(locator).click()
         return ModalWindow(self.driver)
 
     def get_ingredient_counter(self, ingredient_name: str) -> str:
-        locator = (By.XPATH, f"//div[contains(@data-testid, 'ingredient')]//p[text()='{ingredient_name}']/../..//p[contains(@class, 'counter')]")
+        locator = MainPageLocators.ingredient_counter_locator(ingredient_name)
         return self.get_text(locator)
 
     def add_ingredient_to_order(self, ingredient_name: str):
-        ingredient_locator = (By.XPATH, f"//div[contains(@class, 'ingredient')]//p[text()='{ingredient_name}']/..")
-        self.find_element(ingredient_locator).click()
+        locator = MainPageLocators.ingredient_locator(ingredient_name)
+        self.find_element(locator).click()
 
     def place_order(self):
         self.click(MainPageLocators.ORDER_BUTTON)
