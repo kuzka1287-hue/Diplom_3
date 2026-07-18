d2b325b84fc6d7f9f2447744356c57fadeaa1156
kuzka1287-hue <kuzka1287@gmail.com>
2026-07-18 22:16:55 +0300

    Update main_page_locators.py

--- locators/main_page_locators.py
+++ locators/main_page_locators.py
@@ -3,8 +3,14 @@ from selenium.webdriver.common.by import By
 class MainPageLocators:
     CONSTRUCTOR_BUTTON = (By.XPATH, "//a[contains(@href, '/') and contains(text(), 'Конструктор')]")
     ORDERS_FEED_BUTTON = (By.XPATH, "//a[contains(@href, '/feed') and contains(text(), 'Лента заказов')]")
-    INGREDIENT_ITEM = (By.XPATH, "//div[contains(@class, 'ingredient') and contains(@data-testid, 'ingredient')]")
     BUN_COUNTER = (By.XPATH, "//div[contains(@data-testid, 'bun')]//p[contains(@class, 'counter')]")
-    INGREDIENT_COUNTER = (By.XPATH, "//div[contains(@data-testid, 'ingredient')]//p[contains(@class, 'counter')]")
     ORDER_BUTTON = (By.XPATH, "//button[contains(text(), 'Оформить заказ')]")
     ORDER_NUMBER = (By.XPATH, "//div[contains(@class, 'OrderDetails')]//h2")
+
+    @staticmethod
+    def ingredient_locator(ingredient_name: str):
+        return (By.XPATH, f"//div[contains(@class, 'ingredient')]//p[text()='{ingredient_name}']/..")
+
+    @staticmethod
+    def ingredient_counter_locator(ingredient_name: str):
+        return (By.XPATH, f"//div[contains(@data-testid, 'ingredient')]//p[text()='{ingredient_name}']/../..//p[contains(@class, 'counter')]")
