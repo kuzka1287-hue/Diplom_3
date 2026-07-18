331745f8b1f50ebd7b46200bae6462a9c44ae058
kuzka1287-hue <kuzka1287@gmail.com>
2026-07-18 10:59:14 +0300

    Update modal_window.py

--- pages/modal_window.py
+++ pages/modal_window.py
@@ -1,19 +1,13 @@
-# pages/modal_window.py
-
-from selenium.webdriver.common.by import By
 from pages.base_page import BasePage
+from locators.modal_window_locators import ModalWindowLocators
 
 class ModalWindow(BasePage):
-    CLOSE_BUTTON = (By.XPATH, "//button[contains(@class, 'modal__close')]")
-    MODAL_TITLE = (By.XPATH, "//div[contains(@class, 'modal')]//h2")
-    MODAL_CONTENT = (By.XPATH, "//div[contains(@class, 'modal')]//div[contains(@class, 'content')]")
-
     def close_modal(self):
-        self.click(self.CLOSE_BUTTON)
-        self.wait_for_invisibility(self.CLOSE_BUTTON)
+        self.click(ModalWindowLocators.CLOSE_BUTTON)
+        self.wait_for_invisibility(ModalWindowLocators.CLOSE_BUTTON)
 
-    def get_modal_title(self):
-        return self.get_text(self.MODAL_TITLE)
+    def get_modal_title(self) -> str:
+        return self.get_text(ModalWindowLocators.MODAL_TITLE)
 
-    def is_modal_displayed(self):
-        return self.find_element(self.MODAL_TITLE).is_displayed()
+    def is_modal_displayed(self) -> bool:
+        return self.find_element(ModalWindowLocators.MODAL_TITLE).is_displayed()
